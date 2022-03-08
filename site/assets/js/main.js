dca47ee9002fb91ce936b64504349ff8a5185d99
Randy Vianda Putra <modulloe@gmail.com>
2022-03-09 03:35:38 +0700

    style(monitoring): fix feedback ui

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -91,8 +91,6 @@ $(document).ready(function () {
   handleCloseAlert();
   handleSlider();
 
-  
-  
   window.addEventListener("resize", createOffsets);
   
   function createOffsets() {
@@ -148,4 +146,21 @@ $(document).ready(function () {
     }
   
   }
+
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1; 
+  const yyyy = today.getFullYear();
+
+  if (dd < 10) {
+      dd = '0' + dd
+  } 
+
+  if (mm < 10) {
+      mm = '0' + mm
+  } 
+
+  today = yyyy + '-' + mm + '-' + dd;
+  $('.date-monitoring').val(today)
+
 });
